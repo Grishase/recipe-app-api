38d1697688506f5d7738dd84312af90594bd8c59
Grishase <102784645+Grishase@users.noreply.github.com>
2023-04-27 23:10:04 -0400

    Configuring Postgresql in DOCKERFILE

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,6 +4,9 @@ Calculator functions
 
 
 """
-def add(x,y):
+def add(x, y):
     """Add x and y and return result"""
     return x + y
+def subtract(x, y):
+    
+    return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,10 +1,19 @@
 from app import calc
 from django.test import SimpleTestCase
 
+
 class CalcTest(SimpleTestCase):
 
     def test_add_two(self):
 
-        res = calc.add(5,6)
-        
-        self.assertEqual(res,11)
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subctracting numbers """
+        res = calc.subtract(15, 10)
+
+        self.assertEqual(res, 5)
+
+
