21bf54063e884eb689f20cf3251bcb92ceed61b9
Grishase <102784645+Grishase@users.noreply.github.com>
2023-04-29 08:40:56 -0400

    Commited the changes for wait db and migrations.

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,7 +17,5 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('recipe/',include('recipe.urls')),
-
+    path('admin/', admin.site.urls)
 ]
---
+++ app/core/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin # noqa 
+
+"""Tells the flake 8 with noqa to ignore that command """
+
+# Register your models here.
